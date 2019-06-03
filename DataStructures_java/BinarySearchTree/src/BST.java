d09ce9b70e0be7fac20eb56d1c06c819cd7acd1e
lifushuai <lfs736012@163.com>
2019-06-03 14:40:30 +0800

    二叉搜索树

--- DataStructures_java/BinarySearchTree/src/BST.java
+++ DataStructures_java/BinarySearchTree/src/BST.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 public class BST<E extends Comparable<E>> {
 
@@ -42,7 +44,7 @@ public class BST<E extends Comparable<E>> {
 
         if (e.compareTo(node.e) < 0)
             node.left = add(node.left, e);
-        else
+        else if(e.compareTo(node.e) > 0)
             node.right = add(node.right, e);
         return node;
     }
@@ -113,6 +115,126 @@ public class BST<E extends Comparable<E>> {
         System.out.println(node.e);
     }
 
+    public void levelOrder(){
+        if (root == null)
+            return;
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()){
+            Node cur = q.remove();
+            System.out.println(cur.e);
+
+            if (cur.left != null)
+                q.add(cur.left);
+            if (cur.right != null)
+                q.add(cur.right);
+        }
+    }
+
+    public E minimum(){
+        if (size == 0)
+            throw new IllegalArgumentException("BST is empty");
+        Node minNode = minimum(root);
+        return minNode.e;
+    }
+    private Node minimum(Node node){
+        if (node.left == null)
+            return node;
+        return minimum(node.left);
+    }
+
+    public E maximum(){
+        if (size == 0)
+            throw new IllegalArgumentException("BST is empty");
+        return maximum(root).e;
+    }
+
+    private Node maximum(Node node){
+        if (node.right == null)
+            return node;
+        return maximum(node.right);
+    }
+
+    public E removeMin(){
+        E ret = minimum();
+        root = removeMin(root);
+        return ret;
+    }
+    private Node removeMin(Node node){
+        if (node.left == null){
+            Node rightNode = node.right;
+            node.right = null;
+            size--;
+            return rightNode;
+        }
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+    public E removeMax(){
+        E ret = maximum();
+        root =removeMax(root);
+        return ret;
+    }
+
+    // 删除掉以node为根的二分搜索树中的最大节点
+    // 返回删除节点后新的二分搜索树的根
+    private Node removeMax(Node node){
+        if (node.right == null){
+            Node leftNode = node.left;
+            node.left = null;
+            size--;
+            return leftNode;
+        }
+
+        node.right = removeMax(node.right);
+        return node;
+    }
+    public void remove(E e){
+        root = remove(root, e);
+    }
+
+    private Node remove(Node node, E e){
+        if (node == null)
+            return null;
+
+        if (e.compareTo(node.e) < 0){
+            node.left = remove(node.left, e);
+            return node;
+        }
+        else if (e.compareTo(node.e) > 0){
+            node.right = remove(node.right, e);
+            return node;
+        }
+        else {
+            if (node.left == null){
+                Node rightNode = node.right;
+                node.right = null;
+                size --;
+                return rightNode;
+            }
+
+            // 待删除节点右子树为空的情况
+            if (node.right == null){
+                Node leftNode = node.left;
+                node.left = null;
+                size --;
+                return leftNode;
+            }
+            // 待删除节点左右子树均不为空的情况
+
+            // 找到比待删除节点大的最小节点, 即待删除节点右子树的最小节点
+            // 用这个节点顶替待删除节点的位置
+
+            Node successor = minimum(node.right);
+            successor.right = removeMin(node.right);
+            successor.left = node.left;
+            node.left = node.right = null;
+            return successor;
+        }
+    }
+
+
 
     @Override
     public String toString(){
@@ -137,8 +259,5 @@ public class BST<E extends Comparable<E>> {
         for(int i = 0 ; i < depth ; i ++)
             res.append("--");
         return res.toString();
-    }
-
-    public static void main(String[] args) {
     }
 }
